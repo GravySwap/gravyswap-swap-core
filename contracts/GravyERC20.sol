f4f8119b76379f94bdde74298d02912839131fb8
Liquid <Liquid369@gmail.com>
2022-03-03 16:56:58 -0600

    update: fix tests, update truffle

--- contracts/GravyERC20.sol
+++ contracts/GravyERC20.sol
@@ -7,7 +7,7 @@ contract GravyERC20 is IGravyERC20 {
     using SafeMath for uint;
 
     string public constant name = 'Gravy LPs';
-    string public constant symbol = 'Cake-LP';
+    string public constant symbol = 'Gravy-LP';
     uint8 public constant decimals = 18;
     uint  public totalSupply;
     mapping(address => uint) public balanceOf;
